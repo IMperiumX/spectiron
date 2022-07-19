518211121e2a9cbd1712845cc0de2d2423397ce3
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-19 18:21:58 +0200

    restrict what can be imported from the package

--- openapi_specgen/__init__.py
+++ openapi_specgen/__init__.py
@@ -1,5 +1,8 @@
 """Top-level package for openapi-specgen."""
+from .param import OpenApiParam
+from .response import OpenApiResponse
 
+__all__ = ['OpenApiParam', 'OpenApiResponse']
 __author__ = """Yusuf Adel"""
 __email__ = 'yusufadell.dev@gmail.com'
 __version__ = '0.1.0'
