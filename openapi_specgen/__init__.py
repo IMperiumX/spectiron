e5edafac7347f9637421bc70fabf816fcd4fa70a
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-20 16:16:47 +0200

    Adds OpenApiPath

--- openapi_specgen/__init__.py
+++ openapi_specgen/__init__.py
@@ -1,8 +1,9 @@
 """Top-level package for openapi-specgen."""
 from .param import OpenApiParam
 from .response import OpenApiResponse
+from .path import OpenApiPath
 
-__all__ = ['OpenApiParam', 'OpenApiResponse']
+__all__ = ['OpenApiParam', 'OpenApiResponse', 'OpenApiPath']
 __author__ = """Yusuf Adel"""
 __email__ = 'yusufadell.dev@gmail.com'
 __version__ = '0.1.0'
