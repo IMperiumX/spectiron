70ba8cb6be109620703b9c0c5e5dda7a9a2c7736
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-07-27 00:20:36 +0200

    init openapi to be loaded with package import

--- openapi_specgen/__init__.py
+++ openapi_specgen/__init__.py
@@ -1,10 +1,10 @@
 """Top-level package for openapi-specgen."""
-from .openapi_specgen import OpenApi
-from .param import OpenApiParam
-from .path import OpenApiPath
-from .response import OpenApiResponse
+from openapi import OpenApi
+from param import OpenApiParam
+from path import OpenApiPath
+from response import OpenApiResponse
 
-__all__ = ['OpenApiParam', 'OpenApiResponse', 'OpenApiPath']
+__all__ = ['OpenApiParam', 'OpenApiResponse', 'OpenApiPath', 'OpenApi']
 __author__ = """Yusuf Adel"""
 __email__ = 'yusufadell.dev@gmail.com'
 __version__ = '0.1.0'
