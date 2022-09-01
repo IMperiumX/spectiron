107b3c630e494d0e8bb37a6f833bde790d33b3ee
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-01 18:10:11 +0200

    chore(openApiPara): make passing  params optional

--- openapi_specgen/param.py
+++ openapi_specgen/param.py
@@ -1,29 +1,32 @@
 from dataclasses import dataclass
-from .utils import get_openapi_schema
+from typing import Any
+
+from openapi_specgen.utils import get_openapi_schema
 
 
 @dataclass
-class OpenApiParam():
-    name: str
-    location: str
+class OpenApiParam:
+    name: str = ""
+    location: str = ""
     data_type: type = None
-    default: bool = None
+    default: Any = None
     required: bool = True
+    reference: bool = True
 
     def asdict(self):
         openapi_dict = {
-            'required': self.required,
-            'name': self.name,
-            'in': self.location
+            "required": self.required,
+            "name": self.name,
+            "in": self.location,
         }
 
         schema = {}
 
         if self.data_type:
-            schema = get_openapi_schema(self.data_type)
+            schema = get_openapi_schema(self.data_type, self.reference)
         if self.default:
-            schema['default'] = self.default
+            schema["default"] = self.default
 
-        schema['title'] = self.name.title()
-        openapi_dict['schema'] = schema
+        schema["title"] = self.name.title()
+        openapi_dict["schema"] = schema
         return openapi_dict
