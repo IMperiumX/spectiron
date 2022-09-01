88eb652dc5662ff8cfafd71dfeed4cd0933993db
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-01 18:08:16 +0200

    refactor(constants): use constants without import

--- openapi_specgen/__init__.py
+++ openapi_specgen/__init__.py
@@ -4,7 +4,12 @@ from .param import OpenApiParam
 from .path import OpenApiPath
 from .response import OpenApiResponse
 
-__all__ = ['OpenApiParam', 'OpenApiResponse', 'OpenApiPath', 'OpenApi']
+__all__ = [
+    "OpenApiParam",
+    "OpenApiResponse",
+    "OpenApiPath",
+    "OpenApi",
+]
 __author__ = """Yusuf Adel"""
-__email__ = 'yusufadell.dev@gmail.com'
-__version__ = '0.1.0'
+__email__ = "yusufadell.dev@gmail.com"
+__version__ = "0.1.0"
