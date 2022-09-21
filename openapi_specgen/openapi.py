0abf51bee906b99262772cdd7a76d1b1ecd48000
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-21 16:41:09 +0200

    refactor: dict generating logic

--- openapi_specgen/openapi.py
+++ openapi_specgen/openapi.py
@@ -1,48 +1,57 @@
 from dataclasses import dataclass, field
 from typing import List
 
+import openapi_specgen
 from openapi_specgen.path import OpenApiPath
-from openapi_specgen.schema import OPENAPI_DEFAULT_TYPE, get_openapi_schema, get_type
+from openapi_specgen.schema import (OPENAPI_DEFAULT_TYPE, get_openapi_schema,
+                                    get_type)
 
 
 @dataclass
 class OpenApi:
 
-    version: str = "0.1.0"
-    title: str = ""
+    version: str = openapi_specgen.__version__
+    title: str = "OpenApi"
     paths: List[OpenApiPath] = field(default_factory=list)
-    reference: bool = False
+    reference: bool = True
 
     def asdict(self):
         openapi_dict = {
             "openapi": self.version,
             "info": {"title": self.title, "version": self.version},
-            "paths": {},
-            "components": {"schemas": {}},
+            "paths": {path.path: path.asdict() for path in self.paths},
+            "components": {
+                "schemas": {
+                    get_type(schema): get_openapi_schema(schema, self.reference)
+                    for path in self.paths
+                    for schema in path.schemas
+                }
+            },
         }
-        for openapi_path in self.paths:
-            if openapi_dict["paths"].get(openapi_path.path) is None:
-                openapi_dict["paths"][openapi_path.path] = {}
-            openapi_dict["paths"][openapi_path.path].update(
-                openapi_path.asdict()[openapi_path.path]
-            )
 
-            for param in openapi_path.params:
-                if (
-                    get_type(param.data_type, OPENAPI_DEFAULT_TYPE)
-                    == OPENAPI_DEFAULT_TYPE
-                ):
-                    openapi_dict["components"]["schemas"] = get_openapi_schema(
-                        param.data_type
-                    )
+        # for openapi_path in self.paths:
+        #     if openapi_dict["paths"].get(openapi_path.path) is None:
+        #         openapi_dict["paths"][openapi_path.path] = {}
+        #     openapi_dict["paths"][openapi_path.path].update(
+        #         openapi_path.asdict()[openapi_path.path]
+        #     )
 
-            for resp in openapi_path.responses:
-                if (
-                    get_type(resp.data_type, OPENAPI_DEFAULT_TYPE)
-                    == OPENAPI_DEFAULT_TYPE
-                ):
-                    openapi_dict["components"]["schemas"] = get_openapi_schema(
-                        resp.data_type
-                    )
+        #     for param in openapi_path.params:
+        #         if (
+        #             get_type(param.data_type, OPENAPI_DEFAULT_TYPE)
+        #             == OPENAPI_DEFAULT_TYPE
+        #         ):
+        #             openapi_dict["components"]["schemas"] = get_openapi_schema(
+        #                 param.data_type
+        #             )
+
+        #     for resp in openapi_path.responses:
+        #         if (
+        #             get_type(resp.data_type, OPENAPI_DEFAULT_TYPE)
+        #             == OPENAPI_DEFAULT_TYPE
+        #         ):
+        #             openapi_dict["components"]["schemas"] = get_openapi_schema(
+        #                 resp.data_type
+        #             )
 
         return openapi_dict
