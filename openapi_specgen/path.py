6c6dd0f94b8333365b5b5ecc26e15274c757651f
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-21 16:41:43 +0200

    handle request_body param

--- openapi_specgen/path.py
+++ openapi_specgen/path.py
@@ -3,6 +3,7 @@ from typing import List
 
 from openapi_specgen.param import OpenApiParam
 from openapi_specgen.response import OpenApiResponse
+from openapi_specgen.schema import get_openapi_schema
 
 
 @dataclass
@@ -15,11 +16,11 @@ class OpenApiPath:
     # Stackoverflow:https://stackoverflow.com/questions/53632152/why-cant-dataclasses-have-mutable-defaults-in-their-class-attributes-declaratio
     params: List[OpenApiParam] = field(default_factory=list)
 
-    descr: str = ""
-    summary: str = ""
+    descr: str = "Description of the endpoint"
+    summary: str = "Summary of the endpoint"
 
     def asdict(self):
-        return {
+        openapi_dict = {
             self.path: {
                 self.method.upper(): {
                     "description": self.descr,
@@ -34,3 +35,12 @@ class OpenApiPath:
                 }
             }
         }
+        if self.request_body is not None:
+            openapi_dict[self.path][self.method]["requestBody"] = {
+                "content": {
+                    "application/json": {
+                        "schema": get_openapi_schema(self.request_body)
+                    }
+                }
+            }
+        return openapi_dict
