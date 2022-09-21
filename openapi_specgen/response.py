f7e5fcbe3e167a65030b8626e439e20874e47191
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-09-21 16:41:58 +0200

    format with black

--- openapi_specgen/response.py
+++ openapi_specgen/response.py
@@ -5,22 +5,23 @@ from openapi_specgen.schema import get_openapi_schema
 
 @dataclass
 class OpenApiResponse:
-    descr: str
+    descr: str = "OK"
     status_code: int = 200
     data_type: type = None
-    reference: bool = False
+    reference: bool = True
     http_content_type: str = "application/json"
 
     def asdict(self):
         openapi_dict = {str(self.status_code): {"description": self.descr}}
 
-        if self.data_type is None:
-            return openapi_dict
+        if self.data_type is not None:
 
-        openapi_dict[str(self.status_code)]["content"] = {
-            self.http_content_type: {
-                "schema": get_openapi_schema(self.data_type, reference=self.reference)
+            openapi_dict[str(self.status_code)]["content"] = {
+                self.http_content_type: {
+                    "schema": get_openapi_schema(
+                        self.data_type, reference=self.reference
+                    )
+                }
             }
-        }
 
         return openapi_dict
