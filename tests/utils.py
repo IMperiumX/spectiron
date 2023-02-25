332e906fd088eb2f267893cd536963dcb44e0acf
yusufadell <yusufadell.dev@gmail.com>
2023-02-25 11:00:04 +0200

    feat: added DataclassNestedObject in test utils

--- tests/utils.py
+++ tests/utils.py
@@ -1,11 +1,16 @@
-from dataclasses import dataclass
-from typing import List
+from dataclasses import dataclass, field
 
 
 @dataclass
 class DataclassObject:
-    str_field: str
-    int_field: int
-    float_field: float
-    boolean_field: bool
-    list_field: List
+    str_field: str = "test"
+    int_field: int = 1
+    float_field: float = 1.0
+    boolean_field: bool = True
+    list_field: list = field(default_factory=list)
+
+
+@dataclass
+class DataclassNestedObject:
+    str_field: str = "test"
+    nested_object: DataclassObject = field(default_factory=DataclassObject)
