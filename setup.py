b90093c223b1ed9e8707936b956ed5db1bdc07b1
yusufadell <yusufadell.dev@gmail.com>
2023-02-25 10:54:12 +0200

    style: added pre-cimmit hook

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("README.md") as readme_file:
     readme = readme_file.read()
@@ -41,6 +41,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme + "\n\n" + history,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     keywords="openapi_specgen",
     name="openapi_specgen",
