e795e7d47aa27dd10cdfcd963e1b80777f417281
yusufadell <yusufadell.dev@gmail.com>
2023-02-25 10:56:14 +0200

    chore: added OPENAPI_FORMATS combines all formats

--- openapi_specgen/constants.py
+++ openapi_specgen/constants.py
@@ -22,15 +22,22 @@ OPENAPI_ARRAY_ITEM_MAP = {
     list[float]: "number",
     list[int]: "integer",
     list[bool]: "boolean",
+    list[date]: "date",
+    list[datetime]: "date-time",
+    list[list]: "array",
     list: None,
 }
 
+
 get_type = OPENAPI_TYPE_MAP.get
 get_format = OPENAPI_FORMAT_MAP.get
-get_list_generic_type = OPENAPI_ARRAY_ITEM_MAP.get
+get_list_generic = OPENAPI_ARRAY_ITEM_MAP.get
+
+
+OPENAPI_FORMATS = OPENAPI_TYPE_MAP | OPENAPI_FORMAT_MAP | OPENAPI_ARRAY_ITEM_MAP
+
 
 __all__ = [
     "OPENAPI_DEFAULT_TYPE",
     "get_type",
-    "get_list_generic_type",
 ]
